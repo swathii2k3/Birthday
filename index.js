af799a91fd857afe9b125f071cf72b6607aac553
swathii2k3 <swathii.2k3@gmail.com>
2025-11-07 23:54:18 +0530

    sarru

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 function createHeart() {
   const heart = document.createElement('div');
   heart.classList.add('heart');
-  
   heart.style.left = Math.random() * window.innerWidth + 'px';
   heart.style.top = window.innerHeight + 'px';
-  
   document.querySelector('.hearts').appendChild(heart);
-
   setTimeout(() => heart.remove(), 2000);
 }
 setInterval(createHeart, 200);
-
-
-
 const loginForm = document.getElementById('loginForm');
 const passwordInput = document.getElementById('passwordInput');
 const loginContainer = document.querySelector('.login-container');
@@ -28,4 +22,3 @@ loginForm.addEventListener('submit', function(e) {
     alert("Wrong password! Try again 🎈");
   }
 });
-
